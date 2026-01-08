004baaafe7382b31794adb9e9014a12cd2e9b63b
AbdielEscobedo <raulabdielescobedogarcia@gmail.com>
2026-01-08 09:59:29 -0600

    actualizacion

--- server/config/database.js
+++ server/config/database.js
@@ -3,7 +3,10 @@ const path = require('path');
 
 const sequelize = new Sequelize({
   dialect: 'sqlite',
-  storage: path.join(__dirname, '..', 'database.sqlite'),
+  // In Vercel (PROD), use /tmp (ephemeral). In Dev, use local file.
+  storage: process.env.NODE_ENV === 'production'
+    ? path.join('/tmp', 'database.sqlite')
+    : path.join(__dirname, '..', 'database.sqlite'),
   logging: false
 });
 
